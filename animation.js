f07e1d9761b4a44a21b4c469d0a2c81070d1a928
Anna <annalisaschu@live.com>
2019-06-19 23:04:21 +0200

    Meesterproef

---
+++ animation.js
@@ -0,0 +1,159 @@
+document.getElementById('schoonmakericon').addEventListener('click', function(){
+  document.querySelector('.vraag1achtergrond').style.display = 'flex';
+  document.querySelector('.vraag1').style.display = 'flex';
+});
+
+document.getElementById('cross').addEventListener('click', function(){
+  document.querySelector('.vraag1achtergrond').style.display = 'none';
+  document.querySelector('.vraag1').style.display = 'none';
+});
+
+document.getElementById('schoonmakericon2').addEventListener('click', function(){
+  document.querySelector('.vraag2achtergrond').style.display = 'flex';
+  document.querySelector('.vraag2').style.display = 'flex';
+});
+
+document.getElementById('cross2').addEventListener('click', function(){
+  document.querySelector('.vraag2achtergrond').style.display = 'none';
+  document.querySelector('.vraag2').style.display = 'none';
+});
+
+document.getElementById('schoonmakericondrie').addEventListener('click', function(){
+  document.querySelector('.vraag3achtergrond').style.display = 'flex';
+  document.querySelector('.vraag3').style.display = 'flex';
+});
+
+document.getElementById('cross3').addEventListener('click', function(){
+  document.querySelector('.vraag3achtergrond').style.display = 'none';
+  document.querySelector('.vraag3').style.display = 'none';
+});
+
+document.getElementById('schoonmakericonvier').addEventListener('click', function(){
+  document.querySelector('.vraag4achtergrond').style.display = 'flex';
+  document.querySelector('.vraag4').style.display = 'flex';
+});
+
+document.getElementById('cross4').addEventListener('click', function(){
+  document.querySelector('.vraag4achtergrond').style.display = 'none';
+  document.querySelector('.vraag4').style.display = 'none';
+});
+
+
+document.getElementById('een_twee').addEventListener('click', function(){
+  document.getElementById('een_een').style.backgroundColor = "black";
+  document.getElementById('een_een').style.color = "black";
+  document.getElementById('een_een').style.borderColor = "black";
+  document.getElementById('een_drie').style.backgroundColor = "black";
+  document.getElementById('een_drie').style.color = "black";
+  document.getElementById('een_drie').style.borderColor = "black";
+  document.getElementById('een_twee').style.backgroundColor = "#df5b57";
+  document.getElementById('een_twee').style.borderColor = "#df5b57";
+  document.querySelector('.knipper1').style.display = 'flex';
+});
+
+document.getElementById('twee_een').addEventListener('click', function(){
+  document.getElementById('twee_een').style.backgroundColor = "#df5b57";
+  document.getElementById('twee_een').style.borderColor = "#df5b57";
+  document.getElementById('twee_twee').style.backgroundColor = "black";
+  document.getElementById('twee_twee').style.color = "black";
+  document.getElementById('twee_twee').style.borderColor = "black";
+  document.querySelector('.knipper2').style.display = 'flex';
+});
+
+document.getElementById('drie_drie').addEventListener('click', function(){
+  document.getElementById('drie_drie').style.backgroundColor = "#df5b57";
+  document.getElementById('drie_drie').style.borderColor = "#df5b57";
+  document.getElementById('drie_een').style.backgroundColor = "black";
+  document.getElementById('drie_een').style.color = "black";
+  document.getElementById('drie_een').style.borderColor = "black";
+  document.getElementById('drie_twee').style.backgroundColor = "black";
+  document.getElementById('drie_twee').style.color = "black";
+  document.getElementById('drie_twee').style.borderColor = "black";
+  document.querySelector('.knipper3').style.display = 'flex';
+});
+
+document.getElementById('vier_een').addEventListener('click', function(){
+  document.getElementById('vier_een').style.backgroundColor = "#df5b57";
+  document.getElementById('vier_een').style.borderColor = "#df5b57";
+  document.getElementById('vier_twee').style.backgroundColor = "black";
+  document.getElementById('vier_twee').style.color = "black";
+  document.getElementById('vier_twee').style.borderColor = "black";
+  document.getElementById('vier_drie').style.backgroundColor = "black";
+  document.getElementById('vier_drie').style.color = "black";
+  document.getElementById('vier_drie').style.borderColor = "black";
+  document.querySelector('.knipper4').style.display = 'flex';
+});
+
+document.getElementById('infopag1').addEventListener('click', function(){
+  document.querySelector('.verhaal1').style.display = 'flex';
+  document.querySelector('.verhaal1info').style.display = 'flex';
+  document.querySelector('.verhaal1quote').style.display = 'flex';
+  document.querySelector('.meerinfo').style.display = 'flex';
+});
+
+document.getElementById('infopag2').addEventListener('click', function(){
+  document.querySelector('.verhaal2').style.display = 'flex';
+  document.querySelector('.verhaal2info').style.display = 'flex';
+  document.querySelector('.verhaal2quote').style.display = 'flex';
+  document.querySelector('.meerinfo').style.display = 'flex';
+});
+
+document.getElementById('infopag3').addEventListener('click', function(){
+  document.querySelector('.verhaal3').style.display = 'flex';
+  document.querySelector('.verhaal3info').style.display = 'flex';
+  document.querySelector('.verhaal3quote').style.display = 'flex';
+  document.querySelector('.meerinfo').style.display = 'flex';
+});
+
+document.getElementById('infopag4').addEventListener('click', function(){
+  document.querySelector('.verhaal4').style.display = 'flex';
+  document.querySelector('.verhaal4info').style.display = 'flex';
+  document.querySelector('.verhaal4quote').style.display = 'flex';
+  document.querySelector('.meerinfo').style.display = 'flex';
+});
+
+document.getElementById('unlockmeer').addEventListener('click', function(){
+  document.querySelector('.opnieuw').style.display = 'flex';
+  document.querySelector('.verhaal1').style.display = 'none';
+  document.querySelector('.verhaal1info').style.display = 'none';
+  document.querySelector('.verhaal1quote').style.display = 'none';
+  document.querySelector('.meerinfo').style.display = 'none';
+  document.querySelector('.vraag1achtergrond').style.display = 'none';
+  document.querySelector('.vraag1').style.display = 'none';
+  document.querySelector('.verhaal2').style.display = 'none';
+  document.querySelector('.verhaal2info').style.display = 'none';
+  document.querySelector('.verhaal2quote').style.display = 'none';
+  document.querySelector('.vraag2achtergrond').style.display = 'none';
+  document.querySelector('.vraag2').style.display = 'none';
+  document.querySelector('.verhaal3').style.display = 'none';
+  document.querySelector('.verhaal3info').style.display = 'none';
+  document.querySelector('.verhaal3quote').style.display = 'none';
+  document.querySelector('.vraag3achtergrond').style.display = 'none';
+  document.querySelector('.vraag3').style.display = 'none';
+  document.querySelector('.verhaal4').style.display = 'none';
+  document.querySelector('.verhaal4info').style.display = 'none';
+  document.querySelector('.verhaal4quote').style.display = 'none';
+  document.querySelector('.vraag4achtergrond').style.display = 'none';
+  document.querySelector('.vraag4').style.display = 'none';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
